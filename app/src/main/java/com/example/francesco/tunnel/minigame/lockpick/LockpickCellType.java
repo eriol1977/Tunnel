4c5e9f174b9c7c02d07c15f841aa636952cd4f5b
francescobertolino <francesco.bertolino@gmail.com>
2014-12-18 16:03:47 +0100

    storia completa fino all'uscita dalla cella

--- app/src/main/java/com/example/francesco/tunnel/minigame/lockpick/LockpickCellType.java
+++ app/src/main/java/com/example/francesco/tunnel/minigame/lockpick/LockpickCellType.java
@@ -6,12 +6,11 @@ import android.graphics.Color;
  * Created by Francesco on 18/12/2014.
  */
 public enum LockpickCellType {
-    FAR(Color.WHITE, 0), NEAR(Color.YELLOW, 30), NEAREST(Color.RED, 10), HIT(Color.GREEN, 0);
+    FAR(Color.WHITE, 0), NEAR(Color.YELLOW, 100), NEAREST(Color.RED, 10), HIT(Color.GREEN, 0);
 
     private final int color;
 
     private final int vibrationSpeed;
-
 
     LockpickCellType(int color, int vibrationSpeed) {
         this.color = color;
